c4750dd565f0d015975b2cbd5dea48526cf8dda2
Matthew Petroff <matthew@mpetroff.net>
2016-03-19 20:14:09 -0400

    [web] Stay in standalone window on iOS and don't try turning numbers into phone numbers.

---
+++ web/stay-standalone.js
@@ -0,0 +1,3 @@
+// Stay in standalone web app on iOS
+// From https://gist.github.com/irae/1042167
+(function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(chref=d.href).replace(e.href,"").indexOf("#")&&(!/^[a-z\+\.\-]+:/i.test(chref)||chref.indexOf(e.protocol+"//"+e.host)===0)&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone");
